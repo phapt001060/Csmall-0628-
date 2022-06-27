151713d173bf47f22f22096ca6bd7ba5da714213
Administrator <835950568@qq.com>
2022-06-27 18:08:13 +0800

    0627

---
+++ src/main/java/cn/tedu/csmall/server/Jsd2203CsmallServerApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsd2203CsmallServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jsd2203CsmallServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/tedu/csmall/server/Jsd2203CsmallServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jsd2203CsmallServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
